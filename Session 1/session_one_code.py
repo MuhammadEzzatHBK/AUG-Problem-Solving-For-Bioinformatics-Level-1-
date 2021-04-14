7b5ef956690aba1ed23076da6ba72867becfd392
Muhammad Ezzat <muhammadezzat316@gmail.com>
2021-04-14 09:26:25 +0200

    Added First session materials

---
+++ Session 1/session_one_code.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 09:14:52 2021
+
+@author: Muhammad Ezzat
+"""
+# Print Statemnt
+print('Hello World')
+
+# Arthimatics (PEMDAS)
+print(3*(7-2)+1/3)
+
+# Variables
+x = 5
+print(x)
+x +=1
+print(x)
+x= 'Hello World'
+print(x)
+x= False
+print(x)
+
+#Single Line comment
+''' Multi
+        Line
+            Comment'''
+            
