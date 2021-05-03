df18e96048cc3fb3b61a4c0d4873c70a5744ba2e
Ahmed Nasser <60184582+AhmedNasser1601@users.noreply.github.com>
2021-05-03 18:01:18 +0200

    Edit
    Solve an Error

--- Session 4/BioPython/Biopython_Fasta.py
+++
@@ -1,9 +0,0 @@
-from Bio import SeqIO
-from Bio.Seq import Seq
-
-for seq_record in SeqIO.parse("ls_orchid.fasta", "fasta"):
-    print(seq_record.id)
-    print(repr(seq_record.seq))
-    sequence = Seq(seq_record.seq.__str__())
-    print('A:', sequence.count('A'), ', T:', sequence.count('T'), ', G:', sequence.count('G'), ', C:', sequence.count('C'))
-    print('Length is: ', len(seq_record), '\n')
--- Session 4/Packages/Requests.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-x = requests.get('https://w3schools.com/python/demopage.htm')
-print(x. text, '\n')
-#Check If the Page exists
-if x.status_code == 200:
-    print('\tSuccess!')
-elif x.status_code == 404:
-    print('\tNot Found!')
